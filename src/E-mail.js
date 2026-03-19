2b12dffc50dafeb4467be7434db8952a61935b99
Fady Atef <atef13858@gmail.com>
2026-03-19 04:54:00 +0200

    check-2

--- src/E-mail.js
+++ src/E-mail.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Email() {
   return (
-    <div className='Email container m-auto row'>
+    <div className='Email container m-auto row justify-content-between'>
         <div className='EmailChildh2 col-lg-5 col-sm-12 mt-2'>
             <h2 className=''>
                 See my Porjects At once & Leave
                 Here Your E-mail Adress
             </h2>
         </div>
-        <div className='EmailChildinp col-lg-7 col-sm-12'>
+        <div className='EmailChildinp col-lg-5 col-sm-12 d-flex justify-content-between'>
             <input type='text' placeholder='Email Adress' />
             <button className='EmailSub w-25 w-sm-50'>Submit</button>
         </div>
